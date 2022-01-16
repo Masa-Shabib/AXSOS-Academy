8e76f8117b026ca8b8eab86416da3c3af5d7fb6b
Masa Shabib <masashabib@yahoo.com>
2022-01-16 16:17:05 +0200

    .

--- 002 Wep fundamentals/03 JS/06 Liking Likes/part 2/like1.js
+++ 002 Wep fundamentals/03 JS/06 Liking Likes/part 2/like1.js
@@ -3,9 +3,9 @@ var lcount = [
     document.querySelector("#likes1"),
     document.querySelector("#likes2"),
     document.querySelector("#likes3")]
-function increase(id){
-    count[id]++
-    lcount[id].innerText= count[id] +"like(s)" 
+function increase(x){
+    count[x]++
+    lcount[x].innerText= count[x] +" Like(s)" 
 }
 
 
