e2bf185120034e23bd71fd03eb549e7c509ad6a9
Masa Shabib <masashabib@yahoo.com>
2022-01-16 22:20:19 +0200

    .

---
+++ 002 Wep fundamentals/03 JS/07 Make Connection/script.js
@@ -0,0 +1,23 @@
+console.log("page loaded...");
+var newname = document.querySelector(".myname");
+function edit(){
+    newname.innerText = "Masa Shabib";
+}
+var count = 2;
+var lcount = document.querySelector(".badge");
+var hidden = [document.querySelector(".card-list-item"),document.querySelector(".card-list-item0")]
+function hideD(x){
+    hidden[x].remove();
+    count--;
+    lcount.innerText = count 
+}
+var count1 = 500;
+var lcount1 = document.querySelector(".badge1");
+function hideI(x){
+    hidden[x].remove();
+    count1++;
+    lcount1.innerText = count1 
+}
+
+
+
