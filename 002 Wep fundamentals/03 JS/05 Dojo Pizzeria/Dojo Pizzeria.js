b495bd96f7aed6a096f219207e35b85d7719a954
Masa Shabib <masashabib@yahoo.com>
2022-01-16 13:28:18 +0200

    .

--- 002 Wep fundamentals/03 JS/05 Dojo Pizzeria/Dojo Pizzeria.js
+++ 002 Wep fundamentals/03 JS/05 Dojo Pizzeria/Dojo Pizzeria.js
@@ -7,14 +7,59 @@ function pizzaOven(crustType, sauceType, cheese, toppings) {
     return pizza;
 }
     
-var p1 = pizzaOven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]);
-console.log(p1);
-var p2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
-console.log(p2);
-var p3 = pizzaOven("hand tossed", "marinara", "mozzarella","mushrooms");
-console.log(p3);
 
-var crustType ={
+console.log( pizzaOven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]));
+console.log(pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]));
+console.log( pizzaOven("thin", "meyo", "mozzarella","mushrooms"));
+
+var crustType =[
+    "deep dish",
+    "hand tossed",
+    "thin",
+    "gluten free"
+]
+var sauceType =[
+    "traditional",
+    "marinara",
+    "bbq",
+    "meyo"
+];
+var cheese =[
+    "mozzarella",
+    "cheddar",
+    "feta",
+    "swiss cheese",
+    "parmesan"
+];
+var toppings =[
+    "pepperoni",
+    "sausage",
+    "corn",
+    "bell peppers",
+    "mushrooms"
+];
+function randomRange(max, min) {
+    return Math.floor(Math.random() * max - min) + min;
 }
-var sauceType ={
+
+function randomPick(arr) {
+    var i = Math.floor(arr.length * Math.random());
+    return arr[i];
 }
+
+function randomPizza() {
+    var pizza = {};
+    pizza.crustType = randomPick(crustType);
+    pizza.sauceType = randomPick(sauceType);
+    pizza.cheeses = [];
+    pizza.toppings = [];
+    for(var i=0; i<randomRange(4, 1); i++) {
+        pizza.cheeses.push(randomPick(cheese));
+    }
+    for(var i=0; i<randomRange(4, 0); i++) {
+        pizza.toppings.push(randomPick(toppings));
+    }
+    return pizza;
+}
+
+console.log(randomPizza());
