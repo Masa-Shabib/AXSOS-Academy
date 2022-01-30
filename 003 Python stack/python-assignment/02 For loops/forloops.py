a25ebbf4466feb9f5dcc42191b2ce3f6073c211f
Masa Shabib <masashabib@yahoo.com>
2022-01-30 14:36:02 +0200

    .

---
+++ 003 Python stack/python-assignment/02 For loops/forloops.py
@@ -0,0 +1,30 @@
+# for int in range(0,151):
+#     print(int)
+
+# for int in range(5,1001):
+#     if int%5 == 0:
+#         print(int)
+
+# for int in range(1,100):
+#     if int%10 == 0:
+#         print("Coding Dojo")
+#     elif int%5 ==0:
+#         print("Coding")
+#     else:
+#         print(int)
+
+# sum = 0
+# for int in range(0,500001):
+#     if int % 2 !=0:
+#         sum= sum + int
+# print(sum)
+
+# for int in range(2018,0,-4):
+#     print(int)
+
+lowNum=2
+highNum=15
+mult=2
+for int in range(lowNum,highNum+1):
+    if int % mult==0:
+        print(int)
