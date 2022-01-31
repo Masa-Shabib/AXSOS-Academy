0f343aa5ad3e9c88b4cb263ee4c2c588f91ab4ac
Masa Shabib <masashabib@yahoo.com>
2022-01-31 11:59:40 +0200

    .

---
+++ 003 Python stack/python-assignment/08 user/user.py
@@ -0,0 +1,30 @@
+class User:		# here's what we have so far
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account_balance = 0
+    # adding the deposit method
+    def make_deposit(self, amount):
+        self.account_balance += amount
+        print("User:",self.name,",","Balance:",self.account_balance,"$")
+    def make_withdrawal(self, amount):	
+        self.account_balance -= amount
+        print("User:",self.name,",","Balance:",self.account_balance,"$")
+    def  transfer_money(self, other_user, amount) :
+        self.account_balance -= amount
+        other_user.account_balance += amount
+        print("User:",self.name,",","Balance:",self.account_balance,"$")
+        print("User:",other_user.name,",","Balance:",other_user.account_balance,"$")
+
+masa = User("Masa Shabib","masa@yahoo.com")
+marah = User("Marah Shabib","marah@yahoo.com")
+mohammad= User("Mohammad Shabib","mohammad@yahoo.com")
+# masa.make_deposit(100)
+# masa.make_deposit(300)
+# masa.make_deposit(200)
+# masa.make_withdrawal(400)
+# marah.make_deposit(100)
+# marah.make_deposit(300)
+# marah.make_withdrawal(400)
+# marah.make_withdrawal(400)
+masa.transfer_money( marah, 200)
