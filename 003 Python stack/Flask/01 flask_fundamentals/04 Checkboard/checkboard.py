0b816b1ad442c10ae58dbb05ce4857029bc54690
Masa Shabib <masashabib@yahoo.com>
2022-02-04 18:30:11 +0200

    .

--- 003 Python stack/Flask/01 flask_fundamentals/04 Checkboard/checkboard.py
+++ 003 Python stack/Flask/01 flask_fundamentals/04 Checkboard/checkboard.py
@@ -3,13 +3,13 @@ from flask import Flask, render_template
 app= Flask(__name__)
 @app.route("/")
 def hello():
-    return render_template("index.html", i = 4, x= 4,coll="red",col="black")
+    return render_template("index.html", i = 4, x= 4,color1="red",color2="black")
 @app.route("/<int:num>")
 def num1(num):
-    return render_template("index.html", i = 4, x =  int(num/2),coll="red",col="black")
+    return render_template("index.html", i = 4, x =  int(num/2),color1="red",color2="black")
 @app.route("/<int:num>/<int:num1>")
 def num2(num,num1):
-    return render_template("index.html", i =int(num/2), x = int(num1/2),coll="red",col="black")
+    return render_template("index.html", i =int(num/2), x = int(num1/2),color1="red",color2="black")
 @app.route("/<int:num>/<int:num1>/<color1>/<color2>")
 def color(num,num1,color1,color2):
     return render_template("index.html", i =int(num/2), x = int(num1/2),color1=color1,color2=color2)
