b46416c1a236722cd68e1422cd73ab1a814b4866
Masa Shabib <masashabib@yahoo.com>
2022-02-05 11:13:19 +0200

    .

--- 003 Python stack/python-assignment/optional/05 Sl list/singlelist.py
+++ 003 Python stack/python-assignment/optional/05 Sl list/singlelist.py
@@ -15,14 +15,38 @@ class List:
             last = last.next 
         last.next = new_node
         return 
+    def delete(self,index):
+        if self.head == None:
+            return
+        if index == 0:
+            self.head = self.head.next
+            return
+        position = 1
+        # while current is not None:
+        #     if position == index :
+        #         current.next=current.next.next
+        #         position+=1
+        #     return
+    def delete1(self,index):
+        if index == 0:
+            return self.head.next 
+        current = self.head
+        for i in range (index-1):
+            current= current.next
+        current.next = current.next.next
+        return
+    def delete_end(self):
+        last = self.head
+        while last.next is not None :
+            last = last 
+        last = None
+        return
+
     def print(self):
         current = self.head
         while current is not None:
             print (current.value, "---> ",end="")
             current = current.next
-
-
-
 
 my_list = List()
 my_list.add_at_end(141)
@@ -30,5 +54,6 @@ my_list.add_at_end(142)
 my_list.add_at_end(143)
 my_list.add_at_end(144)
 my_list.add_at_end(145)
+my_list.add_at_end(146)
+my_list.delete1(1)
 my_list.print()
-
