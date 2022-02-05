9ffe9531f73511fa954286defac9a685e54337aa
Masa Shabib <masashabib@yahoo.com>
2022-02-05 17:01:16 +0200

    .

--- 003 Python stack/Flask/01 flask_fundamentals/06 Dojo Survey/dojo survey.py
+++ 003 Python stack/Flask/01 flask_fundamentals/06 Dojo Survey/dojo survey.py
@@ -9,7 +9,7 @@ def index():
 def create_user():
     print(request.form['location'])
     return render_template("show.html", name=request.form['name'],location =request.form['location'],
-    language =request.form['lang'],comment =request.form['comment'])
+    language =request.form['lang'],comment =request.form['comment'],jop=request.form['jop'],exp=request.form['exp'])
 
 if __name__ == "__main__":
     app.run(debug=True)
