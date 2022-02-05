3b457bdbc48b9e5ad3cc464e60853e96723a97b9
Masa Shabib <masashabib@yahoo.com>
2022-02-05 16:21:56 +0200

    .

---
+++ 003 Python stack/Flask/01 flask_fundamentals/06 Dojo Survey/dojo survey.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+# our index route will handle rendering our form
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/result', methods=['POST'])
+def create_user():
+    print(request.form['location'])
+    return render_template("show.html", name=request.form['name'],location =request.form['location'],
+    language =request.form['lang'],comment =request.form['comment'])
+
+if __name__ == "__main__":
+    app.run(debug=True)
