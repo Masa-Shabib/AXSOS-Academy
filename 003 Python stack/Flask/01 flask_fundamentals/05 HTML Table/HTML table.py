d3784413cc0b4794525d675041de6d8a2a44c0e6
Masa Shabib <masashabib@yahoo.com>
2022-02-05 13:58:59 +0200

    .

--- 003 Python stack/Flask/01 flask_fundamentals/05 HTML Table/HTML table.py
+++ 003 Python stack/Flask/01 flask_fundamentals/05 HTML Table/HTML table.py
@@ -1,13 +1,13 @@
 from flask import Flask , render_template
 app= Flask(__name__)
 
-@app.route('/lists')
+@app.route('/')
 def render_lists():
     users = [
-   {'first_name' : 'Michael', 'last_name' : 'Choi'},
-   {'first_name' : 'John', 'last_name' : 'Supsupin'},
-   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-   {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    {'first_name' : 'Michael', 'last_name' : 'Choi'},
+    {'first_name' : 'John', 'last_name' : 'Supsupin'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
     return render_template("lists.html", users = users)
 
