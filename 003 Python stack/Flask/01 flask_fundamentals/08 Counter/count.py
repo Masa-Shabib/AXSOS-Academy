27d70da8f5d09577a929ca5f11e22ccdc71927a5
Masa Shabib <masashabib@yahoo.com>
2022-02-06 14:02:45 +0200

    .

--- 003 Python stack/Flask/01 flask_fundamentals/08 Counter/count.py
+++ 003 Python stack/Flask/01 flask_fundamentals/08 Counter/count.py
@@ -1,1 +1,31 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,session
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
+@app.route('/')
+def counter():
+    if not 'count' in session:
+        session['count']=0
+    else :
+        session['count']+=1
+    return render_template("index.html",count=session['count'],num=session['num'])
+@app.route('/destroy_session')
+def destroy():
+    session.pop('count')
+    session['num']=0
+    return redirect('/')
+@app.route('/add2')
+def add2():
+    session['count']+=1
+    return redirect('/')
+@app.route('/num',methods=['POST'])
+def add():
+    session['num']=int(request.form['num'])
+    session['count']+=(session['num']-1)
+    return redirect('/')
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
