826563092cdfd29705e295416bb27c4b59459fcd
Masa Shabib <masashabib@yahoo.com>
2022-03-12 14:14:19 +0200

    .

---
+++ 004 Java/04 SpringBoot/Daikichi_Path_Variables/src/main/java/com/daikichi/DaikichiPathVariablesApplication.java
@@ -0,0 +1,29 @@
+package com.daikichi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DaikichiPathVariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiPathVariablesApplication.class, args);
+	}
+	@RequestMapping("/t/{daikichi}/{travel}/{place}")
+    public String travel(@PathVariable("daikichi") String daikichi, @PathVariable("travel") String travel, @PathVariable("place") String place){
+    	return "Congratulations! You will soon travel to "+ place +"!";
+    }
+	@RequestMapping("/n/{daikichi}/{lotto}/{num}")
+    public String lotto(@PathVariable("daikichi") String daikichi, @PathVariable("lotto") String lotto, @PathVariable("num") String num){
+		int num_int = Integer.parseInt(num);
+		if(num_int%2 ==0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		}else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+    }
+}
+}
